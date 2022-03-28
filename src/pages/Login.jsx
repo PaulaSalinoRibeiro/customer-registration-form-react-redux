ca159795ebb16c6b410b6032de1d127d4ed7a6ee
PAULA RIBEIRO <paulasalinor@gmail.com>
2022-03-28 13:00:00 -0300

    login-form

---
+++ src/pages/Login.jsx
@@ -0,0 +1,44 @@
+import React, { Component } from "react";
+
+class Login extends Component {
+  render() {
+    return (
+      <>
+        <fieldset>
+
+          <label htmlFor="email">
+          Email:
+          {' '}
+          <input
+            id="email"
+            name="email"
+            type="text"
+            required
+          />
+          </label>
+
+          <label htmlFor="senha">
+            Senha:
+            {' '}
+            <input
+              id="senha"
+              name="senha"
+              type="text"
+              required
+            />
+          </label>
+
+          <button
+            type="submit"
+          >
+            Enviar
+          </button>
+
+        </fieldset>
+      
+      </>
+    )
+  }
+}
+
+export default Login;
---
+++ src/pages/RegisterClientes.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class RegisterClients extends Component {
+  render() {
+    return (
+      <>
+      </>
+    )
+  }
+}
+
+export default RegisterClients;
